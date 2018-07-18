87e3fdbbad0aed90586cea02fd6573ff1165d85c
hanyc0914 <hanyc0914@163.com>
2018-07-18 11:03:38 +0800

    add label & button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLabel *label = new QLabel("hello world", this);
+    label->show();
 }
 
 MainWindow::~MainWindow()
